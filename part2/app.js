b9ac93c788d62966adf43ff7a2c58f3517963f54
Rey <1786860383@qq.com>
2025-06-20 19:40:13 +0000

    [automatic commit] 2025-06-20 19:40:13 UTC 58.174.12.223

--- part2/app.js
+++ part2/app.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-// session(stored sata)
+// session(stored station)
 app.use(session({
   secret: 'dogwalk-secret-key',
   resave: false,
