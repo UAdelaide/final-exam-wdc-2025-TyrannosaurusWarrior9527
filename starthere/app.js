06d0bfb97e4360853f4ffc9fec676b3c71e7dcf2
Rey <1786860383@qq.com>
2025-06-20 02:49:57 +0000

    [automatic commit] 2025-06-20 02:49:57 UTC 58.174.12.223

--- starthere/app.js
+++ starthere/app.js
@@ -20,7 +20,7 @@ let db;
     const connection = await mysql.createConnection({
       host: 'localhost',
       user: 'root',
-      password: '' // Set your MySQL root password
+      password: '123456' // Set your MySQL root password
     });
 
     // Create the database if it doesn't exist
