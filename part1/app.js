b9050277f8f134e0b01d7a2c83a25cec741b2ef3
Rey <1786860383@qq.com>
2025-06-20 17:25:43 +0000

    [automatic commit] 2025-06-20 17:25:43 UTC 58.174.12.223

--- part1/app.js
+++ part1/app.js
@@ -30,7 +30,7 @@ let db;
     db = await mysql.createConnection({
       host: 'localhost',
       user: 'root',
-      password: '',
+      password: '123456',
       database: 'DogWalkService'
     });
 
