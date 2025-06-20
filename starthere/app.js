5c3c91d684dc83a1c76122ae2f95272dbeb8db0e
Rey <1786860383@qq.com>
2025-06-20 13:52:29 +0000

    [automatic commit] 2025-06-20 13:52:29 UTC 58.174.12.223

--- starthere/app.js
+++ starthere/app.js
@@ -20,7 +20,7 @@ let db;
       host: process.env.DB_HOST || '127.0.0.1',
       user: process.env.DB_USER || 'root',
       password: process.env.DB_PASSWORD || '123456',
-      database: process.env.DB_NAME || 'DogWalkService'
+      database: process.env.DB_NAME || 'DogWalkService',
       port:3307
     });
     console.log('✅ Connected to MySQL');
