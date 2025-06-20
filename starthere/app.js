1891285ab817e5b120651cd6add39accf940f422
Rey <1786860383@qq.com>
2025-06-20 13:18:55 +0000

    [automatic commit] 2025-06-20 13:18:54 UTC 58.174.12.223

--- starthere/app.js
+++ starthere/app.js
@@ -1,11 +1,11 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var mysql = require('mysql2/promise');
-var bcrypt = require("bcryptjs");
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mysql = require('mysql2/promise');
+const bcrypt = require("bcryptjs");
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
