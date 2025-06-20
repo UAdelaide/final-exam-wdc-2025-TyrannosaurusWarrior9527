53174beee81e5ce2e6e7af914cb2f24eb7fa7c32
Rey <1786860383@qq.com>
2025-06-20 13:37:27 +0000

    [automatic commit] 2025-06-20 13:37:27 UTC 58.174.12.223

--- starthere/app.js
+++ starthere/app.js
@@ -17,7 +17,7 @@ let db;
 (async () => {
   try {
     db = await mysql.createConnection({
-      host: process.env.DB_HOST || 'localhost',
+      host: process.env.DB_HOST || '127.0.0.1',
       user: process.env.DB_USER || 'root',
       password: process.env.DB_PASSWORD || '123456',
       database: process.env.DB_NAME || 'DogWalkService'
