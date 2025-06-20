74b7c903d8f83330795da60a1391560fdcf4d5cc
Rey <1786860383@qq.com>
2025-06-20 14:56:13 +0000

    [automatic commit] 2025-06-20 14:56:13 UTC 58.174.12.223

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -78,4 +78,4 @@ INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, st
 ((SELECT dog_id FROM Dogs WHERE name = 'Bella'), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
 ((SELECT dog_id FROM Dogs WHERE name = 'Lucky'), '2025-06-11 11:00:00', 60, 'Bus station', 'open'),
 ((SELECT dog_id FROM Dogs WHERE name = 'Milo'), '2025-06-12 08:30:00', 30, 'Riverside', 'open'),
-((SELECT dog_id FROM Dogs WHERE name = 'Charlie'), '2025-06-12 18:00:00', 40, 'Suburb Trail', 'open');
+((SELECT dog_id FROM Dogs WHERE name = 'Pumpkin'), '2025-06-12 17:00:00', 50, 'Suburb Trail', 'open');
